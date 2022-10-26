2e7dea945bc3cfb5899aedcf6aff3d349799678f
Colorado Montolio <jorge.colorado-montolio@capgemini.com>
2022-10-26 12:08:17 +0200

    Data formatting

--- app/src/main/java/com/example/customcardsample/ui/CustomCardData.kt
+++ app/src/main/java/com/example/customcardsample/ui/CustomCardData.kt
@@ -2,18 +2,19 @@ package com.example.customcardsample.ui
 
 import com.example.customcardsample.model.card.CardNumber
 import com.example.customcardsample.model.card.CardStatus
-import com.example.customcardsample.model.card.CardType
 
 data class CustomCardData(
     val name: String,
-    val type: CardType,
+    val type: String,
     val number: CardNumber,
     val hiddenNumber: String,
     val titular: Boolean,
     val thumbnail: String,
-    val limit: Double,
-    var disposed: Double,
-    var available: Double,
+    val limit: String,
+    var disposed: String,
+    var currentBalance: Int,
+    var maxBalance: Int,
+    var available: String,
     var status: CardStatus,
     var optionsListener: () -> Unit,
     var activationToggleListener: (Boolean) -> Unit,
